3eb3ff84fa637737d10f5b7998fc0f8d7676ab56
hozza94 <hozza94@users.noreply.github.com>
2021-09-09 14:30:58 +0900

    modify remote server conn

--- churchm/__init__.py
+++ churchm/__init__.py
@@ -40,4 +40,7 @@ def create_app():
     app.register_blueprint(member_views.bp)
     app.register_blueprint(auth_views.bp)
 
+    if __name__ == "__main__":
+        app.run(host='0.0.0.0', debug=True)
+
     return app
