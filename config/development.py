ba83ce2bb4d0b667f90edb94bec119aa465829c7
hozza94 <hozza94@users.noreply.github.com>
2021-09-09 19:34:21 +0900

    modify spell

--- config/development.py
+++ config/development.py
@@ -1,5 +1,5 @@
 from config.default import *
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'churchm.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "dev"
